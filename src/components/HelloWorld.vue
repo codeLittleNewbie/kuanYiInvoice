738cf9e9da36a963f1ac4cfbd9ae2200531a4bdb
codeLittleNewbie <935814110@qq.com>
2017-12-14 17:19:38 +0800

    模板更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,68 +1,20 @@
 <template>
   <div class="hello">
     <!--<mt-header fixed title="固定在顶部"></mt-header>-->
+    <ky-header fixed>
 
-    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
-      <router-link to="/" slot="left">
-        <mt-button icon="back">返回</mt-button>
-      </router-link>
-      <mt-button icon="more" slot="right"></mt-button>
-    </mt-header>
+    </ky-header>
 
-    <mt-button type="default" @click="openMsg">default</mt-button>
-    <mt-button type="primary" @click="openDate" size="small">打开日历</mt-button>
-    <mt-button type="primary" size="small">{{selected}}</mt-button>
-
-
-    <mt-tabbar v-model="selected">
-      <mt-tab-item id="外卖">
-        <img slot="icon" src="../assets/logo.png">
-        外卖
-      </mt-tab-item>
-      <mt-tab-item id="订单">
-        <img slot="icon" src="../assets/logo.png">
-        订单
-      </mt-tab-item>
-      <mt-tab-item id="发现">
-        <img slot="icon" src="../assets/logo.png">
-        发现
-      </mt-tab-item>
-      <mt-tab-item id="我的">
-        <img slot="icon" src="../assets/logo.png">
-        我的
-      </mt-tab-item>
-    </mt-tabbar>
-
-
-    <!--日历-->
-    <template>
-      <mt-datetime-picker
-        ref="picker"
-        type="date"
-        v-model="pickerValue">
-      </mt-datetime-picker>
-    </template>
   </div>
 </template>
 
 <script>
 
-  import { MessageBox } from 'mint-ui';
+  import kyHeader from '@/common/header/Header.vue'
 
   export default {
-    data(){
-      return {
-        pickerValue: '2017-10-12',
-        selected: ''
-      }
-    },
-    methods: {
-      openMsg(){
-        MessageBox('提示','操作成功');
-      },
-      openDate(){
-        this.$refs.picker.open();
-      }
+    components: {
+      kyHeader
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,20 +5,13 @@ import App from './App'
 import router from './router'
 import {
   Button,
-  Cell,
-  DatetimePicker,
   Header,
-  Tabbar, TabItem
 } from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 // mint-ui按需就不能使用use方式了->tip
-Vue.component(Button.name, Button);
-Vue.component(Cell.name, Cell);
-Vue.component(DatetimePicker.name, DatetimePicker);
 Vue.component(Header.name, Header);
-Vue.component(Tabbar.name, Tabbar);
-Vue.component(TabItem.name, TabItem);
+Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false
 
